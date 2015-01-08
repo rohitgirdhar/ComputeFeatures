25e70e69f0d38deb76739c2db81e062d85af25d2
Rohit Girdhar <rgirdhar@andrew.cmu.edu>
2015-01-08 17:49:18 -0500

    working with C++

--- Features/CNN/computeFeatures.cpp
+++ Features/CNN/computeFeatures.cpp
@@ -18,7 +18,7 @@ namespace fs = boost::filesystem;
 
 void genImgsList(const fs::path&, vector<fs::path>&);
 template<typename Dtype>
-void computeFeatures(Net<Dtype>&, const vector<Mat>&, string, vector<vector<Dtype>>&);
+void computeFeatures(Net<Dtype>&, const vector<Mat>&, string, int, vector<vector<Dtype>>&);
 void dumpFeature(fs::path, const vector<float>&);
 
 int
@@ -29,6 +29,7 @@ main(int argc, char *argv[]) {
     #else
     Caffe::set_mode(Caffe::GPU);
     #endif
+    Caffe::set_phase(Caffe::TEST); // important, else will give random features
     
     po::options_description desc("Allowed options:");
     desc.add_options()
@@ -40,7 +41,8 @@ main(int argc, char *argv[]) {
         fs::path("/home/rgirdhar/work/03_temp/caffe_dev/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel");
     string LAYER = "pool5";
     fs::path OUTDIR = fs::path("output");
-    fs::path IMGSDIR = fs::path(".");
+    fs::path IMGSDIR = fs::path("imgs/PeopleAtLandmarks/corpus/");
+    int BATCH_SIZE = 50; // read it from NetParameters
  
     NetParameter test_net_params;
     ReadProtoFromTextFile(NETWORK_PATH.string(), &test_net_params);
@@ -59,12 +61,17 @@ main(int argc, char *argv[]) {
 
     vector<Mat> Is;
     for (auto imgpath : imgs) {
-        Mat I = imread((IMGSDIR / imgpath).string());
+        Mat I = imread(imgpath.string());
+        if (!I.data) {
+            LOG(ERROR) << "Unable to read image " << imgpath;
+            break;
+        }
         resize(I, I, Size(256, 256));
         Is.push_back(I);
     }
+    LOG(INFO) << "read images";
     vector<vector<float>> output;
-    computeFeatures<float>(caffe_test_net, Is, LAYER, output);
+    computeFeatures<float>(caffe_test_net, Is, LAYER, BATCH_SIZE, output);
 
     // Dump output
     for (int i = 0; i < imgs.size(); i++) {
@@ -81,15 +88,34 @@ template<typename Dtype>
 void computeFeatures(Net<Dtype>& caffe_test_net,
         const vector<Mat>& imgs,
         string LAYER,
+        int BATCH_SIZE,
         vector<vector<Dtype>>& output) {
-    vector<int> dvl(imgs.size(), 0);
-    boost::dynamic_pointer_cast<caffe::MemoryDataLayer<Dtype>>(
-            caffe_test_net.layers()[0])->AddMatVector(imgs, dvl);
-    vector<Blob<Dtype>*> dummy_bottom_vec;
-    Dtype loss = 0.0f;
-    caffe_test_net.Forward(dummy_bottom_vec, &loss);
-    const boost::shared_ptr<Blob<Dtype>> feat = caffe_test_net.blob_by_name(LAYER);
-    output.push_back(vector<Dtype>(feat->cpu_data(), feat->cpu_data() + feat->count()));
+    int nImgs = imgs.size();
+    int nBatches = ceil(nImgs * 1.0f / BATCH_SIZE);
+    for (int batch = 0; batch < nBatches; batch++) {
+        int actBatchSize = min(nImgs - batch * BATCH_SIZE, BATCH_SIZE);
+        vector<Mat> imgs_b;
+        if (actBatchSize >= BATCH_SIZE) {
+            imgs_b.insert(imgs_b.end(), imgs.begin() + batch * BATCH_SIZE, 
+                    imgs.begin() + (batch + 1) * BATCH_SIZE);
+        } else {
+            imgs_b.insert(imgs_b.end(), imgs.begin() + batch * BATCH_SIZE, imgs.end());
+            for (int j = actBatchSize; j < BATCH_SIZE; j++)
+                imgs_b.push_back(imgs[0]);
+        }
+        vector<int> dvl(BATCH_SIZE, 0);
+        boost::dynamic_pointer_cast<caffe::MemoryDataLayer<Dtype>>(
+                caffe_test_net.layers()[0])->AddMatVector(imgs_b, dvl);
+        vector<Blob<Dtype>*> dummy_bottom_vec;
+        Dtype loss = 0.0f;
+        caffe_test_net.ForwardPrefilled(&loss);
+        const boost::shared_ptr<Blob<Dtype>> feat = caffe_test_net.blob_by_name(LAYER);
+        for (int i = 0; i < actBatchSize; i++) {
+            Dtype* feat_data = feat->mutable_cpu_data() + feat->offset(i);
+            output.push_back(vector<Dtype>(feat_data, feat_data + feat->count() / feat->num()));
+        }
+        LOG(INFO) << "Batch " << batch << " (" << actBatchSize << " images) done";
+    }
 }
 
 void genImgsList(const fs::path& imgsDir, vector<fs::path>& list) {
@@ -109,10 +135,10 @@ void genImgsList(const fs::path& imgsDir, vector<fs::path>& list) {
 }
 
 void dumpFeature(fs::path outpath, const vector<float>& feat) {
-    ofstream fout(outpath.string().c_str(), ios::out);
+    FILE* fout = fopen(outpath.string().c_str(), "w");
     for (auto feati : feat) {
-        fout << feati << endl;
+        fprintf(fout, "%f\n", feati);
     }
-    fout.close();
+    fclose(fout);
 }
 
